4beb8d2d8a5536ea46f032cf07df11e517205ad8
Grace McGinnis <grace.mcg89@gmail.com>
2014-04-14 21:12:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umrails::Application.config.secret_token = '0f7816082532ec273066de31bfe2d00a5cce9743b0fbdfe4bdbe941cce018d9296b7d55f0a2a769c7b60f5631fa1e0d84dd1b2ec76db3f8f6080fc319d1a4a4d'
